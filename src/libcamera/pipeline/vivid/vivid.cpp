a08d108b28442d182db3b7a88f14ef4ebb13da2d
Kieran Bingham <kieran.bingham@ideasonboard.com>
2020-07-10 15:13:05 +0100

    libcamera: pipeline: Introduce skeleton Vivid Pipeline
    Provide all of the skeleton stubs to succesfully compile
    and register a new Pipeline Handler for the Vivid test device.
    
    Meson must be reconfigured to ensure that this pipeline handler is
    included in the selected pipelines configuration, and after building, we
    can test that the PipelineHandler is successfully registered by listing
    the cameras on the system with LIBCAMERA_LOG_LEVELS enabled:
    
    """
    LIBCAMERA_LOG_LEVELS=Pipeline:0 ./build-vivid/src/cam/cam -l
    [230:30:03.624102821] [2867886] DEBUG Pipeline pipeline_handler.cpp:680 Registered pipeline handler "PipelineHandlerVivid"
    Available cameras:
    """
    
    Signed-off-by: Kieran Bingham <kieran.bingham@ideasonboard.com>

---
+++ src/libcamera/pipeline/vivid/vivid.cpp
@@ -0,0 +1,79 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Google Inc.
+ *
+ * vivid.cpp - Pipeline handler for the vivid capture device
+ */
+
+#include <libcamera/base/log.h>
+
+#include "libcamera/internal/pipeline_handler.h"
+
+namespace libcamera {
+
+LOG_DEFINE_CATEGORY(VIVID)
+
+class PipelineHandlerVivid : public PipelineHandler
+{
+public:
+	PipelineHandlerVivid(CameraManager *manager);
+
+	CameraConfiguration *generateConfiguration(Camera *camera,
+						   const StreamRoles &roles) override;
+	int configure(Camera *camera, CameraConfiguration *config) override;
+
+	int exportFrameBuffers(Camera *camera, Stream *stream,
+			       std::vector<std::unique_ptr<FrameBuffer>> *buffers) override;
+
+	int start(Camera *camera, const ControlList *controls) override;
+	void stopDevice(Camera *camera) override;
+
+	int queueRequestDevice(Camera *camera, Request *request) override;
+
+	bool match(DeviceEnumerator *enumerator) override;
+};
+
+PipelineHandlerVivid::PipelineHandlerVivid(CameraManager *manager)
+	: PipelineHandler(manager)
+{
+}
+
+CameraConfiguration *PipelineHandlerVivid::generateConfiguration(Camera *camera,
+								 const StreamRoles &roles)
+{
+	return nullptr;
+}
+
+int PipelineHandlerVivid::configure(Camera *camera, CameraConfiguration *config)
+{
+	return -1;
+}
+
+int PipelineHandlerVivid::exportFrameBuffers(Camera *camera, Stream *stream,
+					     std::vector<std::unique_ptr<FrameBuffer>> *buffers)
+{
+	return -1;
+}
+
+int PipelineHandlerVivid::start(Camera *camera, const ControlList *controls)
+{
+	return -1;
+}
+
+void PipelineHandlerVivid::stopDevice(Camera *camera)
+{
+}
+
+int PipelineHandlerVivid::queueRequestDevice(Camera *camera, Request *request)
+{
+	return -1;
+}
+
+bool PipelineHandlerVivid::match(DeviceEnumerator *enumerator)
+{
+	return false;
+}
+
+REGISTER_PIPELINE_HANDLER(PipelineHandlerVivid)
+
+} /* namespace libcamera */
